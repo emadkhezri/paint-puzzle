b86b07958568ecf4060212be29a78fcae3a27a6e
Emad Khezri <emad.khezri@gmail.com>
2019-05-25 23:15:12 +1000

    White entries have been added to the MixTable dictionary.

--- Assets/Scripts/TileColorMixer.cs
+++ Assets/Scripts/TileColorMixer.cs
@@ -13,34 +13,39 @@ namespace com.paintpuzzle
         {
             //Initialize the mixing table
             s_mixTable = new Dictionary<MixInput, TileColor>();
-
             #region Fill the table
-            s_mixTable.Add(new MixInput(TileColor.White, TileColor.White), TileColor.White);
 
+            s_mixTable.Add(new MixInput(TileColor.White, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.White, TileColor.Red), TileColor.Red);
             s_mixTable.Add(new MixInput(TileColor.White, TileColor.Green), TileColor.Green);
-            s_mixTable.Add(new MixInput(TileColor.White, TileColor.Blue), TileColor.Blue);
+           s_mixTable.Add(new MixInput(TileColor.White, TileColor.Blue), TileColor.Blue);
 
+            s_mixTable.Add(new MixInput(TileColor.Red, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Red, TileColor.Red), TileColor.Red);
             s_mixTable.Add(new MixInput(TileColor.Red, TileColor.Green), TileColor.Yellow);
             s_mixTable.Add(new MixInput(TileColor.Red, TileColor.Blue), TileColor.Magenta);
 
+            s_mixTable.Add(new MixInput(TileColor.Green, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Green, TileColor.Red), TileColor.Yellow);
             s_mixTable.Add(new MixInput(TileColor.Green, TileColor.Green), TileColor.Green);
             s_mixTable.Add(new MixInput(TileColor.Green, TileColor.Blue), TileColor.Cyan);
 
+            s_mixTable.Add(new MixInput(TileColor.Blue, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Blue, TileColor.Red), TileColor.Magenta);
             s_mixTable.Add(new MixInput(TileColor.Blue, TileColor.Green), TileColor.Cyan);
             s_mixTable.Add(new MixInput(TileColor.Blue, TileColor.Blue), TileColor.Blue);
 
+            s_mixTable.Add(new MixInput(TileColor.Yellow, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Yellow, TileColor.Red), TileColor.Yellow);
             s_mixTable.Add(new MixInput(TileColor.Yellow, TileColor.Green), TileColor.Yellow);
             s_mixTable.Add(new MixInput(TileColor.Yellow, TileColor.Blue), TileColor.White);
 
+            s_mixTable.Add(new MixInput(TileColor.Magenta, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Magenta, TileColor.Red), TileColor.Magenta);
             s_mixTable.Add(new MixInput(TileColor.Magenta, TileColor.Green), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Magenta, TileColor.Blue), TileColor.Magenta);
 
+            s_mixTable.Add(new MixInput(TileColor.Cyan, TileColor.White), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Cyan, TileColor.Red), TileColor.White);
             s_mixTable.Add(new MixInput(TileColor.Cyan, TileColor.Green), TileColor.Cyan);
             s_mixTable.Add(new MixInput(TileColor.Cyan, TileColor.Blue), TileColor.Cyan);
